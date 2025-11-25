98acc12c87e370e14f8b7ab97322c60213432918
OscarArantegui <aranteguijimenez24@iespabloserrano.com>
2025-11-25 11:11:08 +0100

    Añadimos campos calculados (asientos ocupados)

--- addons/openacademy/models/session.py
+++ addons/openacademy/models/session.py
@@ -24,3 +24,22 @@ class Session(models.Model):
 
     # 3. Asistentes: Relación Many2many con Contactos (res.partner)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+    # Buscamos instructores (instructor=True) O que tengan una categoría que contenga "Teacher"
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
+        domain=['|', ('instructor', '=', True),
+                     ('category_id.name', 'ilike', "Teacher")])
+    
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+    # CAMPO CALCULADO NUEVO
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
