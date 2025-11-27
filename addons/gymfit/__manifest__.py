b66710b6834159baaacc4152b3b3d66d40d6c3e8
OscarArantegui <aranteguijimenez24@iespabloserrano.com>
2025-11-28 00:18:30 +0100

    GymFit

--- addons/gymfit/__manifest__.py
+++ addons/gymfit/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': "vetshop",
+    'name': "gymfit",
 
     'summary': "Short (1 phrase/line) summary of the module's purpose",
 
@@ -8,7 +8,7 @@ Long description of module's purpose
     """,
 
     'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -21,7 +21,7 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        #'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
